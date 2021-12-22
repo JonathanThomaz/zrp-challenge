c593627ba6c442387289f6033106b0972607a8a2
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2021-12-22 07:50:57 -0300

    feat: add endpoint pokemon with param pokemon

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,32 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
+const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon'
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/pokemon/:pokemon', async (req, res) => {
+    const queryPokemon = req.params.pokemon
+    try {
+        const response = await axios.get(`${pokeApiUrl}/${queryPokemon}`)
+        let arrayAbilities = []
+        if (response.data.abilities) {
+            response.data.abilities.forEach(ability => {
+                arrayAbilities.push(ability.ability.name)
+            })
+        }
+        arrayAbilities.sort((a, b) => {
+            if (a.toUpperCase() < b.toUpperCase()) {
+                return -1
+            }
+        });
+        res.send(arrayAbilities)    
+    } catch (error) {
+        res.send(error).status(500)        
+    }
 })
 
 app.listen(port, () => {
